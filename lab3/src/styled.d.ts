155fc44dc96bed4daada5fdf16571a2638dcf9d4
Riplochelik <riplochelik@gmail.com>
2026-05-09 16:11:42 +0300

    Clean lab4 branch

--- lab3/src/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components/native';
-import { lightTheme } from './theme/theme';
-
-type AppTheme = typeof lightTheme;
-
-declare module 'styled-components/native' {
-  export interface DefaultTheme extends AppTheme {}
-}
